c9eaede5007daa9108f61e91ad88711a765adc3b
Shay Konradsdottir <60191340+sk261@users.noreply.github.com>
2020-02-19 14:49:22 -0800

    Add files via upload
    graphics_modified is a modified version of graphics.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from graphics_modified import *
+'''
+1) Load all the files of the directories into a tree
+2) Organize branches of the tree by sizes of the files (leaves)
+3) Draw tree, giving space for leaves
+4) Draw leaves based on file size
+
+'''
+blankImage = Image(320, 240)
+blankImage.save("blank.gif")
