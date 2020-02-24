43401dd5f78a92e503aeb6f86a1d9e901a9f4734
sk261 <sk261@humboldt.edu>
2020-02-23 22:18:31 -0800

    Full tree builder script with sample output

--- file_loader.py
+++ file_loader.py
@@ -1,42 +1,96 @@
 import os
 
+
+class fileTree:
+    def __init__(self, path):
+        self.trunk = branch(path, 0)
+        self.minName = self.trunk.nameVal
+        self.maxName = self.trunk.nameVal
+        self._buildtree(self.trunk)
+        self.trunk.sizeRatio = 1.0
+
+    def _buildtree(self, br):
+        branchSize = 0
+        for p in os.listdir(br.path):
+            b = branch(os.path.join(br.path, p), br.level + 1)
+            if b.nameVal < self.minName:
+                self.minName = b.nameVal
+            if b.nameVal > self.maxName:
+                self.maxName = b.nameVal
+            if not b.isFile:
+                self._buildtree(b)
+            branchSize += b.size
+            br.branches.append(b)
+        br.size = branchSize
+        if br.size > 0:
+            for p in br.branches:
+                p.sizeRatio = p.size / br.size
+
+        
 class branch:
-    def __init__(self, name):
-        self._name = name
-        self._size = 0
-        self._lastAccessed = 0
-        self._files = []
-        self._branches = []
+    def __init__(self, path, level):
+        self.path = os.path.abspath(path)
+        self.name = os.path.basename(self.path)
+        self.isFile = os.path.isfile(self.path)
+        self.level = level
+        self.nameVal = 0.0
+        self.sizeRatio = 1
+        try:
+            for n in range(len(self.name)):
+                self.nameVal += ord(self.name[n])/(1000.0**n)
+        except:
+            pass
+
+        if self.isFile:
+            self.size = os.stat(path).st_size
+        else:
+            self.size = 0
+        self.branches = []
     def __iter__(self):
         return branch_iter(self)
-
-    @property
-    def size(self):
-        return self._size
-    @size.setter
-    def size(self, value):
-        self._size = value
 
 
 class branch_iter:
     def __init__(self, branch):
-        self._branch = branch
+        self._branch = [branch] + self._makebranch(branch)
         self._index = 0
+
+    def _makebranch(self, branch):
+        ret = branch.branches
+        for x in range(len(branch.branches)-1, -1, -1):
+            n = branch.branches[x]
+            if not n.isFile:
+                br = self._makebranch(n)
+                ret = ret[0:(x+1)] + br + ret[(x+1):]
+        return ret
+        
     def __next__(self):
         if (self._index < len(self._branch)):
             self._index += 1
             return self._branch[self._index - 1]
         raise StopIteration
 
-class fileTree:
-    def __init__(self, path):
-        self.trunk
-
+def printTree(outputFile, tree):
+    leaves = 0
+    branches = 0
+    f = open(outputFile, 'w')
+    for a in tree.trunk:
+        if a.isFile:
+            leaves += 1
+        else:
+            branches += 1
+        toprint = ""
+        for b in range(a.level):
+            toprint += "\t"
+        nameRatio = (a.nameVal-tree.minName)/(tree.maxName-tree.minName)
+        toprint += ','.join([a.name, str(a.size), str(a.sizeRatio), str(nameRatio), a.path])
+        f.write(toprint + "\n")
+    f.close()
+    print("Branches: " + str(branches))
+    print("Leaves: " + str(leaves))
 
 if __name__ == "__main__":
-    x = branch("test")
-    x.size = 5
-    a = x.size
-    print(a)
-    x.size += 5
-    print(x.size)
+    print("Start")
+    x = fileTree("C:\\Users\\Shay\\Desktop")
+    printTree("output.txt", x)
+    print("Done")
--- main.py
+++ main.py
@@ -8,25 +8,42 @@ python -m venv venv
 2) Organize branches of the tree by sizes of the files
 3) Draw tree, length of branch/leaf by file size or folder contents sizes
 
+Output as text document, indented by level, with the following information as csv:
+    Branch Name, Branch Size (in bytes), Branch Size Ratio relative to parent (0.0 to 1.0),
+        Branch Name Ratio relative to whole tree (0.0 to 1.0), Branch Full Path
 
+How to run:
+
+Python main.py <Location to directory or file>... out:<outputfilename>
+Output will be in separate output files as "<outputfilename>_<Location to directory basename>.txt"
+The default <outputfilename> is "output"
+There was very little testing done.
 
 '''
 
 from graphics_modified import *
 import sys
-from file_loader import fileTree
+from file_loader import fileTree, printTree
 
-def makeTree(path):
+def makeTree(path, output):
     tree = fileTree(path)
+    printTree(output, tree)
+
+output = "output"
+trees = []
 
 if len(sys.argv) > 0:
     for path in sys.argv:
+        if path.startswith("out:"):
+            output = path[4:]
         if os.path.isdir(path):
             if os.path.exists(path):
-                makeTree(path)
-else:
-    makeTree('./')
-    
+                trees.append(path)
+if len(trees) == 0:
+    trees.append('./')
+
+for n in trees:
+    makeTree(n, output + "_" + os.path.basename(n) + ".txt")
 
 
 
