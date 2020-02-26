39386d6763344ea3cc5e80791c94561248078db0
sk261 <sk261@humboldt.edu>
2020-02-26 13:46:33 -0800

    formatting out spaces, replaced with tabs

--- file_loader.py
+++ file_loader.py
@@ -1,106 +1,106 @@
 import os
 
 class fileTree:
-    def __init__(self, path):
-        self.trunk = branch(path, 0)
-        self.minName = self.trunk.nameVal
-        self.maxName = self.trunk.nameVal
-        self.minModTime = self.trunk.modTime
-        self.maxModTime = self.trunk.modTime
-        self._buildtree(self.trunk)
-        self.trunk.sizeRatio = 1.0
+	def __init__(self, path):
+		self.trunk = branch(path, 0)
+		self.minName = self.trunk.nameVal
+		self.maxName = self.trunk.nameVal
+		self.minModTime = self.trunk.modTime
+		self.maxModTime = self.trunk.modTime
+		self._buildtree(self.trunk)
+		self.trunk.sizeRatio = 1.0
 
-    def _buildtree(self, br):
-        branchSize = 0
-        try:
-            for p in os.listdir(br.path):
-                b = branch(os.path.join(br.path, p), br.level + 1)
-                if b.nameVal < self.minName:
-                    self.minName = b.nameVal
-                if b.nameVal > self.maxName:
-                    self.maxName = b.nameVal
-                if b.modTime < self.minModTime:
-                    self.minModTime = b.modTime
-                if b.modTime > self.maxModTime:
-                    self.maxModTime = b.modTime
-                if not b.isFile:
-                    self._buildtree(b)
-                branchSize += b.size
-                br.branches.append(b)
-        except:
-            print("Unable to access: " + br.path);
-        br.size = branchSize
-        if br.size > 0:
-            for p in br.branches:
-                p.sizeRatio = p.size / br.size
+	def _buildtree(self, br):
+		branchSize = 0
+		try:
+			for p in os.listdir(br.path):
+				b = branch(os.path.join(br.path, p), br.level + 1)
+				if b.nameVal < self.minName:
+					self.minName = b.nameVal
+				if b.nameVal > self.maxName:
+					self.maxName = b.nameVal
+				if b.modTime < self.minModTime:
+					self.minModTime = b.modTime
+				if b.modTime > self.maxModTime:
+					self.maxModTime = b.modTime
+				if not b.isFile:
+					self._buildtree(b)
+				branchSize += b.size
+				br.branches.append(b)
+		except:
+			print("Unable to access: " + br.path);
+		br.size = branchSize
+		if br.size > 0:
+			for p in br.branches:
+				p.sizeRatio = p.size / br.size
 
-        
+		
 class branch:
-    def __init__(self, path, level):
-        self.path = os.path.abspath(path)
-        self.name = os.path.basename(self.path)
-        self.isFile = os.path.isfile(self.path)
-        self.level = level
-        self.nameVal = 0.0
-        self.modTime = os.path.getmtime(self.path)
-        self.sizeRatio = 1
-        try:
-            for n in range(len(self.name)):
-                self.nameVal += ord(self.name[n])/(1000.0**n)
-        except:
-            pass
+	def __init__(self, path, level):
+		self.path = os.path.abspath(path)
+		self.name = os.path.basename(self.path)
+		self.isFile = os.path.isfile(self.path)
+		self.level = level
+		self.nameVal = 0.0
+		self.modTime = os.path.getmtime(self.path)
+		self.sizeRatio = 1
+		try:
+			for n in range(len(self.name)):
+				self.nameVal += ord(self.name[n])/(1000.0**n)
+		except:
+			pass
 
-        if self.isFile:
-            self.size = os.stat(self.path).st_size
-        else:
-            self.size = 0
-        self.branches = []
-    def __iter__(self):
-        return branch_iter(self)
+		if self.isFile:
+			self.size = os.stat(self.path).st_size
+		else:
+			self.size = 0
+		self.branches = []
+	def __iter__(self):
+		return branch_iter(self)
 
 
 class branch_iter:
-    def __init__(self, branch):
-        self._branch = [branch] + self._makebranch(branch)
-        self._index = 0
+	def __init__(self, branch):
+		self._branch = [branch] + self._makebranch(branch)
+		self._index = 0
 
-    def _makebranch(self, branch):
-        ret = branch.branches
-        for x in range(len(branch.branches)-1, -1, -1):
-            n = branch.branches[x]
-            if not n.isFile:
-                br = self._makebranch(n)
-                ret = ret[0:(x+1)] + br + ret[(x+1):]
-        return ret
-        
-    def __next__(self):
-        if (self._index < len(self._branch)):
-            self._index += 1
-            return self._branch[self._index - 1]
-        raise StopIteration
+	def _makebranch(self, branch):
+		ret = branch.branches
+		for x in range(len(branch.branches)-1, -1, -1):
+			n = branch.branches[x]
+			if not n.isFile:
+				br = self._makebranch(n)
+				ret = ret[0:(x+1)] + br + ret[(x+1):]
+		return ret
+		
+	def __next__(self):
+		if (self._index < len(self._branch)):
+			self._index += 1
+			return self._branch[self._index - 1]
+		raise StopIteration
 
 def printTree(outputFile, tree):
-    leaves = 0
-    branches = 0
-    f = open(outputFile, 'w')
-    for a in tree.trunk:
-        if a.isFile:
-            leaves += 1
-        else:
-            branches += 1
-        toprint = ""
-        for b in range(a.level):
-            toprint += "\t"
-        nameRatio = (a.nameVal-tree.minName)/(tree.maxName-tree.minName)
-        datetimeRatio = (a.modTime-tree.minModTime)/(tree.maxModTime-tree.minModTime)
-        toprint += ','.join([a.name, str(format(a.size,'.0f')), str(format(a.sizeRatio, '.30f')), str(format(nameRatio, '.30f')), str(format(datetimeRatio, '.30f')), a.path, str(int(a.isFile))])
-        f.write(toprint + "\n")
-    f.close()
-    print("Branches: " + str(branches))
-    print("Leaves: " + str(leaves))
+	leaves = 0
+	branches = 0
+	f = open(outputFile, 'w')
+	for a in tree.trunk:
+		if a.isFile:
+			leaves += 1
+		else:
+			branches += 1
+		toprint = ""
+		for b in range(a.level):
+			toprint += "\t"
+		nameRatio = (a.nameVal-tree.minName)/(tree.maxName-tree.minName)
+		datetimeRatio = (a.modTime-tree.minModTime)/(tree.maxModTime-tree.minModTime)
+		toprint += ','.join([a.name, str(format(a.size,'.0f')), str(format(a.sizeRatio, '.30f')), str(format(nameRatio, '.30f')), str(format(datetimeRatio, '.30f')), a.path, str(int(a.isFile))])
+		f.write(toprint + "\n")
+	f.close()
+	print("Branches: " + str(branches))
+	print("Leaves: " + str(leaves))
 
 if __name__ == "__main__":
-    print("Start")
-    x = fileTree("C:\\Users\\Shay\\Desktop")
-    printTree("output.txt", x)
-    print("Done")
+	print("Start")
+	x = fileTree("C:\\Users\\Shay\\Desktop")
+	printTree("output.txt", x)
+	print("Done")
--- main.py
+++ main.py
@@ -4,14 +4,14 @@ python -m venv venv
 .\venv\Scripts\Activate.ps1
 
 1) Load all the files of the directories into a tree
-    file_loader.py
+	file_loader.py
 2) Organize branches of the tree by sizes of the files
 3) Draw tree, length of branch/leaf by file size or folder contents sizes
 
 Output as text document, indented by level, with the following information as csv:
-    Branch Name, Branch Size (in bytes), Branch Size Ratio relative to parent (0.0 to 1.0),
-        Branch Name Ratio relative to whole tree (0.0 to 1.0), Modified Time Ratio relative to whole tree (0.0 to 1.0),
-        Branch Full Path, IsFile (1 or 0)
+	Branch Name, Branch Size (in bytes), Branch Size Ratio relative to parent (0.0 to 1.0),
+		Branch Name Ratio relative to whole tree (0.0 to 1.0), Modified Time Ratio relative to whole tree (0.0 to 1.0),
+		Branch Full Path, IsFile (1 or 0)
 
 How to run:
 
@@ -27,24 +27,24 @@ import os
 from file_loader import fileTree, printTree
 
 def makeTree(path, output):
-    tree = fileTree(path)
-    printTree(output, tree)
+	tree = fileTree(path)
+	printTree(output, tree)
 
 output = "output"
 trees = []
 
 if len(sys.argv) > 0:
-    for path in sys.argv:
-        if path.startswith("out:"):
-            output = path[4:]
-        if os.path.isdir(path):
-            if os.path.exists(path):
-                trees.append(path)
+	for path in sys.argv:
+		if path.startswith("out:"):
+			output = path[4:]
+		if os.path.isdir(path):
+			if os.path.exists(path):
+				trees.append(path)
 if len(trees) == 0:
-    trees.append('./')
+	trees.append('./')
 
 for n in trees:
-    makeTree(n, output + "_" + os.path.basename(n) + ".txt")
+	makeTree(n, output + "_" + os.path.basename(n) + ".txt")
 
 
 
